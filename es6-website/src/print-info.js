06ce8615bebc5c3a01afa5a595333aa327936969
Gastón I. Silva <givanse@gmail.com>
2015-06-10 02:18:28 -0500

    comments and CSS styles

--- es6-website/src/print-info.js
+++ es6-website/src/print-info.js
@@ -1,6 +1,7 @@
 
 function printInfo() {
-  $('#info').append('<p>minimal website example with Broccoli and Babel</p>');
+  $('#info').append('<p>Minimal website example<br>' + 
+                    'with Broccoli and Babel.</p>');
 }
 
 $(printInfo);
