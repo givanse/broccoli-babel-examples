aece1cec7d9c02577682e9b844950662af0fe78e
Chris Galardi <christophergalardi@gmail.com>
2015-07-16 13:59:06 -0400

    remove hardcoded separaters, conform to standards

--- es6-website/Brocfile.js
+++ es6-website/Brocfile.js
@@ -14,12 +14,11 @@ var mergeTrees = require('broccoli-merge-trees');
 var appJs = babel('src');
 
 // Grab the polyfill file provided by the Babel library
-// Grab the polyfill file provided by the Babel library
 var babelPath = require.resolve('broccoli-babel-transpiler');
 babelPath = path.dirname(babelPath);
-babelPath = path.join(babelPath, '/node_modules/babel-core');
+babelPath = path.join(babelPath, 'node_modules', 'babel-core');
 var browserPolyfill = funnel(babelPath, {
-	files: ['browser-polyfill.js']
+  files: ['browser-polyfill.js']
 });
 
 // Add the Babel polyfill to the tree of transpiled files
